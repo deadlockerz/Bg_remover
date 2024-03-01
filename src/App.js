214afdce0ac9c01c76d09b4293165c538e1ec33b
nitin7213 <nitineon123@gmail.com>
2024-03-01 19:23:44 +0530

    env

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
-import Nav from "./components/Header"
+import Header from './components/Header';
+import Footer from './components/Footer';
+import Home from './pages/Home';
 
 const App = () => {
   return (
     <>
-      <Nav></Nav>
+      <Header />
+      <Home />
+      <Footer />
     </>
   );
 };
