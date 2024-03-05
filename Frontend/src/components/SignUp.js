133808fa9d8ce4f8afffd10d77559f67d66f8757
nitin7213 <nitineon123@gmail.com>
2024-03-05 22:27:27 +0530

    correct

--- Frontend/src/components/SignUp.js
+++ Frontend/src/components/SignUp.js
@@ -1,31 +1,37 @@
 import logo from '../assets/logo.png';
 import { Link } from 'react-router-dom';
 import { useState } from 'react';
-import axios from 'axios' // use to post the data
+import axios from 'axios'; // Import axios to post the data
 import { useNavigate } from 'react-router-dom';
 import { useForm } from 'react-hook-form';
 
-
-
 const SignUp = () => {
-   const [name, setName] = useState()
-   const [email, setEmail] = useState()
-   const [password, setPassword] = useState()
-   const navigate = useNavigate()
-   const { register, handleSubmit, formState: { errors } } = useForm();
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const navigate = useNavigate();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
 
-const onSubmit = (data) => {
-  console.log(data);
-};
-   const handleSignUp =(e)=>{
-    e.preventDefault() //check it once
-    axios.post('http://localhost:3030/signup',{ name , email , password })  // send data to server side
-    .then(result => {console.log(result)
-    navigate('/login')
-    })
-    .catch(err => console.log(err))
-   } 
-  return ( 
+  const onSubmit = (data) => {
+    console.log(data);
+  };
+
+  const handleSignUp = (e) => {
+    e.preventDefault();
+    axios
+      .post('http://localhost:3030/signup', { name, email, password }) // Send data to server side
+      .then((result) => {
+        console.log(result);
+        navigate('/login');
+      })
+      .catch((err) => console.log(err));
+  };
+
+  return (
     <>
       <div className='select-none flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8'>
         <div className='sm:mx-auto sm:w-full sm:max-w-sm'>
@@ -35,12 +41,12 @@ const onSubmit = (data) => {
             alt='Your Company'
           />
           <h2 className='mt-5 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900'>
-            Sign in to your account
+            Sign up for an account
           </h2>
         </div>
 
         <div className='mt-7 sm:mx-auto sm:w-full sm:max-w-sm'>
-          <form 
+          <form
             onSubmit={handleSubmit(onSubmit)}
             className='space-y-6'
             action='#'
@@ -48,20 +54,20 @@ const onSubmit = (data) => {
           >
             <div>
               <label
-                htmlFor='email'
+                htmlFor='name'
                 className='block text-sm font-medium leading-6 text-gray-900'
               >
                 Full Name
               </label>
               <div className='mt-2'>
                 <input
-                  onChange={(e) => setName(e.target.value)} // to take the value
+                  onChange={(e) => setName(e.target.value)}
                   id='name'
                   name='name'
                   type='text'
                   autoComplete='name'
                   required
-                  className='px-3  block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-gray-600 sm:text-sm sm:leading-6'
+                  className='px-3 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-gray-600 sm:text-sm sm:leading-6'
                 />
               </div>
             </div>
@@ -75,96 +81,66 @@ const onSubmit = (data) => {
               </label>
               <div className='mt-2'>
                 <input
-                  onChange={(e) => setEmail(e.target.value)} // to take the value
-
+                  onChange={(e) => setEmail(e.target.value)}
                   id='email'
                   name='email'
                   type='email'
                   autoComplete='email'
                   required
-                  className='px-3  block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-gray-600 sm:text-sm sm:leading-6'
+                  className='px-3 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-gray-600 sm:text-sm sm:leading-6'
                 />
               </div>
             </div>
 
             <div>
-              <div className='flex items-center justify-between'>
-                <label
-                  htmlFor='password'
-                  className='block text-sm font-medium leading-6 text-gray-900'
-                >
-                  Password
-                </label>
-              </div>
+              <label
+                htmlFor='password'
+                className='block text-sm font-medium leading-6 text-gray-900'
+              >
+                Password
+              </label>
               <div className='mt-2'>
                 <input
-                                  onChange={(e) => setPassword(e.target.value)} // to take the value
-                                  {...register("password", {
-        required: 'Password is required',
-        minLength: {
-          value: 8,
-          message: 'Password must be at least 8 characters long'
-        },
-        pattern: {
-          value: /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/,
-          message: 'Password must contain at least one uppercase letter, one lowercase letter, and one number'
-        }
-      })}
+                  onChange={(e) => setPassword(e.target.value)}
                   id='password'
                   name='password'
                   type='password'
-                  autoComplete='current-password'
+                  autoComplete='new-password'
                   required
-                  className='px-3  block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-gray-600 sm:text-sm sm:leading-6'
+                  {...register('password', {
+                    required: 'Password is required',
+                    minLength: {
+                      value: 8,
+                      message: 'Password must be at least 8 characters long',
+                    },
+                    pattern: {
+                      value:
+                        /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/,
+                      message:
+                        'Password must contain at least one uppercase letter, one lowercase letter, and one number',
+                    },
+                  })}
+                  className='px-3 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-gray-600 sm:text-sm sm:leading-6'
                 />
                 {errors.password && <p>{errors.password.message}</p>}
               </div>
             </div>
 
-            <div>
-              <div className='flex items-center justify-between'>
-                <label
-                  htmlFor='password'
-                  className='block text-sm font-medium leading-6 text-gray-900'
-                >
-                  Confirm Password
-                </label>
-              </div>
-              <div className='mt-2'>
-                <input
-      //           onChange={(e) => setPassword(e.target.value)}
-      //             {...register("confirmPassword", {
-      //   required: 'Please confirm your password',
-      //   validate: (value) =>
-      //     value === password.current || 'The passwords do not match'
-      // })}
-                  id='Confirm_password'
-                  name='Confirm_password'
-                  type='password'
-                  autoComplete='Confirm_password'
-                  required
-                  className=' px-3 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-gray-600 sm:text-sm sm:leading-6'
-                />
-                {/* {errors.confirmPassword && <p>{errors.confirmPassword.message}</p>} */}
-              </div>
-            </div>
-
-            <div className='flex flex-col '>
+            <div className='flex flex-col'>
               <button
                 type='submit'
                 className='flex w-full justify-center rounded-md bg-gray-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-gray-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-gray-600 mb-2'
               >
                 Sign Up
               </button>
-              </div>
+            </div>
           </form>
 
-
           <p className='mt-10 text-center text-sm text-gray-500'>
-            Already have an account?
+            Already have an account?{' '}
             <Link
               to='/login'
-              className='ml-1 font-semibold leading-6 text-gray-600 hover:text-gray-500'
+              className='font-semibold leading-6 text-gray-600 hover:text-gray-500'
             >
               Log in
             </Link>
