5f98724eba48fe084a4b02e4336e5b2ad2ed18bb
Nitin7213 <nitineon@gmail.com>
2024-03-28 00:05:29 +0530

    frontend

--- Frontend/src/components/SignUp.js
+++ Frontend/src/components/SignUp.js
@@ -1,141 +1,138 @@
-import logo from '../assets/logo.png';
-import { useState } from 'react';
-import axios from 'axios';
-import { useNavigate } from 'react-router-dom';
-import { Link } from 'react-router-dom';
+import logo from "../assets/logo.png";
+import { useState } from "react";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 const SignUp = () => {
-  const [name, setName] = useState('');
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
   const navigate = useNavigate();
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    axios.post('http://localhost:3030/signup', { name, email, password })
-      .then(result => {
+    axios
+      .post("http://localhost:3030/signup", { name, email, password })
+      .then((result) => {
         console.log(result);
-        navigate('/login');
+        navigate("/login");
       })
-      .catch(err => console.log(err));
+      .catch((err) => console.log(err));
   };
 
   return (
     <>
-      <div className='select-none flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8'>
-        <div className='sm:mx-auto sm:w-full sm:max-w-sm'>
-          <img
-            className='mx-auto h-10 w-auto'
-            src={logo}
-            alt='Your Company'
-          />
-          <h2 className='mt-5 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900'>
+      <div className="select-none flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
+        <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+          <img className="mx-auto h-10 w-auto" src={logo} alt="Your Company" />
+          <h2 className="mt-5 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
             Sign up for an account
           </h2>
         </div>
 
-        <div className='mt-7 sm:mx-auto sm:w-full sm:max-w-sm'>
+        <div className="mt-7 sm:mx-auto sm:w-full sm:max-w-sm">
           <form
             onSubmit={handleSubmit}
-            className='space-y-6'
-            action='#'
-            method='POST'
+            className="space-y-6"
+            action="#"
+            method="POST"
           >
             <div>
               <label
-                htmlFor='name'
-                className='block text-sm font-medium leading-6 text-gray-900'
+                htmlFor="name"
+                className="block text-sm font-medium leading-6 text-gray-900"
               >
                 Full Name
               </label>
-              <div className='mt-2'>
+              <div className="mt-2">
                 <input
                   onChange={(e) => setName(e.target.value)}
-                  id='name'
-                  name='name'
-                  type='text'
-                  autoComplete='name'
+                  id="name"
+                  name="name"
+                  type="text"
+                  autoComplete="name"
                   required
-                  className='px-3 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-gray-600 sm:text-sm sm:leading-6'
+                  className="px-3 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-gray-600 sm:text-sm sm:leading-6"
                 />
               </div>
             </div>
 
             <div>
               <label
-                htmlFor='email'
-                className='block text-sm font-medium leading-6 text-gray-900'
+                htmlFor="email"
+                className="block text-sm font-medium leading-6 text-gray-900"
               >
                 Email address
               </label>
-              <div className='mt-2'>
+              <div className="mt-2">
                 <input
                   onChange={(e) => setEmail(e.target.value)}
-                  id='email'
-                  name='email'
-                  type='email'
-                  autoComplete='email'
+                  id="email"
+                  name="email"
+                  type="email"
+                  autoComplete="email"
                   required
-                  className='px-3 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-gray-600 sm:text-sm sm:leading-6'
+                  className="px-3 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-gray-600 sm:text-sm sm:leading-6"
                 />
               </div>
             </div>
 
             <div>
               <label
-                htmlFor='password'
-                className='block text-sm font-medium leading-6 text-gray-900'
+                htmlFor="password"
+                className="block text-sm font-medium leading-6 text-gray-900"
               >
                 Password
               </label>
-              <div className='mt-2'>
+              <div className="mt-2">
                 <input
                   onChange={(e) => setPassword(e.target.value)}
-                  id='password'
-                  name='password'
-                  type='password'
-                  autoComplete='current-password'
+                  id="password"
+                  name="password"
+                  type="password"
+                  autoComplete="current-password"
                   required
-                  className='px-3 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-gray-600 sm:text-sm sm:leading-6'
+                  className="px-3 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-gray-600 sm:text-sm sm:leading-6"
                 />
               </div>
             </div>
 
             <div>
               <label
-                htmlFor='password'
-                className='block text-sm font-medium leading-6 text-gray-900'
+                htmlFor="password"
+                className="block text-sm font-medium leading-6 text-gray-900"
               >
-                Canform Password
+                Canfirm Password
               </label>
-              <div className='mt-2'>
+              <div className="mt-2">
                 <input
                   // onChange={(e) => setPassword(e.target.value)}
-                  id='password'
-                  name='password'
-                  type='password'
-                  autoComplete='current-password'
+                  id="password"
+                  name="password"
+                  type="password"
+                  autoComplete="current-password"
                   required
-                  className='px-3 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-gray-600 sm:text-sm sm:leading-6'
+                  className="px-3 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-gray-600 sm:text-sm sm:leading-6"
                 />
               </div>
             </div>
 
-            <div className='flex flex-col'>
+            <div className="flex flex-col">
               <button
-                type='submit'
-                className='flex w-full justify-center rounded-md bg-gray-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-gray-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-gray-600 mb-2'
+                type="submit"
+                className="flex w-full justify-center rounded-md bg-gray-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-gray-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-gray-600 mb-2"
               >
                 Sign Up
               </button>
             </div>
           </form>
 
-          <p className='mt-10 text-center text-sm text-gray-500'>
+          <p className="mt-10 text-center text-sm text-gray-500">
             Already have an account?
             <Link
-              to='/login'
-              className='ml-1 font-semibold leading-6 text-gray-600 hover:text-gray-500'
+              to="/login"
+              className="ml-1 font-semibold leading-6 text-gray-600 hover:text-gray-500"
             >
               Log in
             </Link>
@@ -152,7 +149,6 @@ export default SignUp;
 // import logo from '../assets/logo.png';
 // import { Link } from 'react-router-dom';
 // import axios from "axios" //to call api and send data
-
 
 // const SignUp = () => {
 
@@ -264,7 +260,7 @@ export default SignUp;
 //                 />
 //               </div>
 //             </div>
-            
+
 //             <div>
 //               <div className='flex items-center justify-between'>
 //                 <label
