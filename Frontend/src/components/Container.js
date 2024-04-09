7a8a70d85c1024e1386908e0b75847ff75939ec5
nitin7213 <nitineon123@gmail.com>
2024-04-09 17:09:33 +0530

    .env update

--- Frontend/src/components/Container.js
+++ Frontend/src/components/Container.js
@@ -25,7 +25,7 @@ const Main = () => {
   useEffect(() => {
     if (!image) return;
 
-    const apikey = "RWLHGmgSGxeHdwrrXB73DpZD";
+    const apikey = process.env.REACT_APP_API_KEY;
     const url = "https://api.remove.bg/v1.0/removebg";
 
     const formData = new FormData();
--- Frontend/src/index.js
+++ Frontend/src/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import Router from "./router/Router";
-import { RouterProvider } from "react-router-dom";
+import { RouterProvider } from "react-router-dom"; //Usually loaded at starting
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
